9691678a846fb3954e8f1bc5e69cedeef9e761d1
Saurabh <saurabh@zautoai.com>
2024-05-06 23:05:16 +0530

    logger added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { StartService } from './start.service';
+import { logger } from './logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
   await startService.createAdmin();
   app.setGlobalPrefix('api');
   app.enableCors();
+  app.use(logger);
   await app.listen(3001);
 }
 bootstrap();
